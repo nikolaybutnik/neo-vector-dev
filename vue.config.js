c0af00c47921c16c5b08ff973562f572c15f1c5f
Nikolay Butnik <btnk.nik@gmail.com>
2025-01-02 15:40:56 -0500

    Setup SCSS

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      scss: {
+        // Optional: Add global SCSS imports here
+        additionalData: `@import "@/styles/variables.scss";`,
+      },
+    },
+  },
 })
