108abd7fe16fd0c53b7372cb074fa8d394747c5f
Nikolay Butnik <btnk.nik@gmail.com>
2025-01-09 19:43:00 -0500

    Exploration of SVG masking

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,19 @@
           </div>
         </div>
       </section>
-      <section class="content-2">Test Content 2</section>
+      <section class="content-2">
+        <div class="spikes">
+          <div class="wrapper">
+            <h1>Test 2 Title</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Dignissimos aliquam, totam ab nobis optio necessitatibus ipsum.
+              Dicta, obcaecati quibusdam cumque, quos natus, cum atque deserunt
+              totam ipsa voluptate id eaque!
+            </p>
+          </div>
+        </div>
+      </section>
       <section class="content-3">Test Content 3</section>
     </div>
   </div>
@@ -58,12 +70,6 @@ export default defineComponent({
   color: $text-color;
   scroll-snap-type: y mandatory;
   overflow-y: scroll;
-  // display: grid;
-  // grid-template-areas: "stack";
-
-  // & > * {
-  //   grid-area: stack;
-  // }
 
   .content-1,
   .content-2,
@@ -76,15 +82,48 @@ export default defineComponent({
     align-items: center;
   }
 
-  .content-1 {
-  }
-
   .content-2 {
     background-color: #2e2e2e;
   }
 
   .content-3 {
     background-color: #494949;
+  }
+
+  .spikes {
+    position: relative;
+    background: linear-gradient(to right, #fdc830, #f37335);
+    color: black;
+    padding: 7rem 3rem;
+
+    &::before,
+    &::after {
+      content: "";
+      position: absolute;
+      width: 100%;
+      height: 50px;
+      background: #2e2e2e;
+      -webkit-mask-image: url("./assets/triangle.svg");
+      -webkit-mask-size: 20px 50px;
+      // to avoid having to use mask-repeat,mask-size height needs to be same as container height
+      // -webkit-mask-repeat: repeat-x;
+
+      // For Firefox and Safari
+      mask-image: url("./assets/triangle.svg");
+      mask-size: 20px 50px;
+      // mask-repeat: repeat-x;
+    }
+
+    &::before {
+      top: 0;
+      left: 0;
+    }
+
+    &::after {
+      bottom: 0;
+      left: 0;
+      transform: rotate(0.5turn);
+    }
   }
 }
 </style>
